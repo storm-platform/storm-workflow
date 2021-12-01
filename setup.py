58dd993541e0546c7095e5b054f0bd17a19210f2
Phelps <phelpsmemo@gmail.com>
2021-12-01 08:19:17 -0300

    update dependencies and invenio entrypoints

--- setup.py
+++ setup.py
@@ -5,36 +5,52 @@
 # storm-pipeline is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Pipeline manager module for the Storm platform"""
+"""Pipeline manager module for the Storm platform."""
 
 import os
 
 from setuptools import find_packages, setup
 
-readme = open('README.rst').read()
-history = open('CHANGES.rst').read()
+readme = open("README.rst").read()
+history = open("CHANGES.rst").read()
 
 tests_require = [
-    'pytest-invenio>=1.4.0',
+    "pytest-invenio>=1.4.0",
 ]
+
+invenio_db_version = ">=1.0.9,<2.0.0"
+invenio_search_version = ">=1.4.2,<2.0.0"
+
 
 extras_require = {
-    'docs': [
-        'Sphinx>=3,<4',
+    "docs": [
+        "Sphinx>=3,<4",
     ],
-    'tests': tests_require,
+    "tests": tests_require,
+    # Elasticsearch
+    "elasticsearch7": [
+        f"invenio-search[elasticsearch7]{invenio_search_version}",
+    ],
+    # Databases
+    "mysql": [
+        f"invenio-db[mysql,versioning]{invenio_db_version}",
+    ],
+    "postgresql": [
+        f"invenio-db[postgresql,versioning]{invenio_db_version}",
+    ],
+    "sqlite": [
+        f"invenio-db[versioning]{invenio_db_version}",
+    ],
 }
 
-extras_require['all'] = []
-for reqs in extras_require.values():
-    extras_require['all'].extend(reqs)
+extras_require["all"] = [req for _, reqs in extras_require.items() for req in reqs]
 
-setup_requires = [
-    'Babel>=2.8',
-]
+setup_requires = []
 
 install_requires = [
-    'invenio-i18n>=1.2.0',
+    "invenio-drafts-resources>=0.14.0,<0.15.0",
+    "invenio-records-resources>=0.17.0,<0.18",
+    "storm-project @ git+https://github.com/storm-platform/storm-project",
 ]
 
 packages = find_packages()
@@ -42,35 +58,46 @@ packages = find_packages()
 
 # Get the version string. Cannot be done with import!
 g = {}
-with open(os.path.join('storm_pipeline', 'version.py'), 'rt') as fp:
+with open(os.path.join("storm_pipeline", "version.py"), "rt") as fp:
     exec(fp.read(), g)
-    version = g['__version__']
+    version = g["__version__"]
 
 setup(
-    name='storm-pipeline',
+    name="storm-pipeline",
     version=version,
     description=__doc__,
-    long_description=readme + '\n\n' + history,
-    keywords='invenio TODO',
-    license='MIT',
-    author='Felipe Menino Carlos',
-    author_email='felipe.carlos@inpe.br',
-    url='https://github.com/storm-platform/storm-pipeline',
+    long_description=readme + "\n\n" + history,
+    keywords=["Storm Platform", "Research pipelines", "Invenio module"],
+    license="MIT",
+    author="Felipe Menino Carlos",
+    author_email="felipe.carlos@inpe.br",
+    url="https://github.com/storm-platform/storm-pipeline",
     packages=packages,
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
+    platforms="any",
     entry_points={
-        'invenio_base.apps': [
-            'storm_pipeline = storm_pipeline:StormPipeline',
+        "invenio_config.module": [
+            "storm_pipeline = storm_pipeline.config",
         ],
-        'invenio_base.blueprints': [
-            'storm_pipeline = storm_pipeline.views:blueprint',
+        "invenio_base.apps": [
+            "storm_pipeline = storm_pipeline:StormPipeline",
         ],
-        'invenio_i18n.translations': [
-            'messages = storm_pipeline',
+        "invenio_base.api_apps": [
+            "storm_pipeline = storm_pipeline:StormPipeline",
         ],
-        # TODO: Edit these entry points to fit your needs.
+        "invenio_base.api_blueprints": [
+            "storm_pipeline_api = storm_pipeline.views:create_research_pipeline_blueprint_api"
+        ],
+        "invenio_db.models": [
+            "pipeline_record = storm_pipeline.pipeline.records.models"
+        ],
+        "invenio_search.mappings": [
+            "pipeline = storm_pipeline.pipeline.records.mappings"
+        ],
+        "invenio_jsonschemas.schemas": [
+            "pipeline = storm_pipeline.pipeline.records.jsonschemas"
+        ]
         # 'invenio_access.actions': [],
         # 'invenio_admin.actions': [],
         # 'invenio_assets.bundles': [],
@@ -87,18 +114,18 @@ setup(
     setup_requires=setup_requires,
     tests_require=tests_require,
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Development Status :: 1 - Planning',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 1 - Planning",
     ],
 )
